1ed684c6cdc5d2145ab9455b969bd3e254b3d327
Your Name <your.email@example.com>
2025-06-06 19:35:22 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import '../styles/globals.css';
 import Header from '../components/Header';
-import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
   return (
     <>
-      {router.pathname !== '/' && <Header />}
-      <div className={router.pathname !== '/' ? 'main-content-padding' : ''}>
+      <Header />
+      <div className="main-content-padding">
         <Component {...pageProps} />
       </div>
     </>
