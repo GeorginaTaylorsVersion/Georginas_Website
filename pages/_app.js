67c781708eb52bd78e50692b90fd806c5d5d58b9
Your Name <your.email@example.com>
2025-06-06 20:44:34 -0400

    8

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
       <div className="main-content-padding">
         <Component {...pageProps} />
       </div>
+      <Footer />
     </>
   );
 }
