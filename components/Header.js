540bc938e8961897df20b9ac22600ff0626816fd
Your Name <your.email@example.com>
2025-06-10 15:56:08 -0400

    3

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <header className={styles['header-bar']} style={{ backgroundColor: 'red' }}>
+    <header className={styles['header-bar']}>
       <div className={styles['nav-content']}>
         <div className={styles.name}>
           Georgina's Website <span style={{ marginLeft: '0.5rem' }} role="img" aria-label="heart">❤️</span>
--- pages/index.js
+++ pages/index.js
@@ -17,34 +17,36 @@ export default function Home({ allTermsData }) {
       <Head>
         <title>Georgina&apos;s Notes</title>
       </Head>
-      <section className="text-xl leading-8 pt-1">
-        <p className="text-xl">
-          Hi! This is the home for all of my notes on my courses.
-        </p>
-      </section>
-      <section className="text-xl leading-8 pt-1 md:text-xl">
-        <h2 className="text-2xl my-4">Terms</h2>
-        <ul className="list-none p-0 m-0">
-          {allTermsData.map(({ id, date, title, courses }) => (
-            <li className="mb-5" key={id}>
-              <Link href={`/terms/${id}`}>
-                <a className="text-blue-500 hover:underline">{title}</a>
-              </Link>
-              <br />
-              <small className="text-gray-500">
-                {date}
+      <div className="homepage-content-padding">
+        <section className="text-xl leading-8 pt-1">
+          <p className="text-xl">
+            Hi! This is the home for all of my notes on my courses.
+          </p>
+        </section>
+        <section className="text-xl leading-8 pt-1 md:text-xl">
+          <h2 className="text-2xl my-4">Terms</h2>
+          <ul className="list-none p-0 m-0">
+            {allTermsData.map(({ id, date, title, courses }) => (
+              <li className="mb-5" key={id}>
+                <Link href={`/terms/${id}`}>
+                  <a className="text-blue-500 hover:underline">{title}</a>
+                </Link>
                 <br />
-                {courses.map((course, index) => (
-                  <span key={course} className="mr-2">
-                    {course}
-                    {index < courses.length - 1 ? ',' : ''}
-                  </span>
-                ))}
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+                <small className="text-gray-500">
+                  {date}
+                  <br />
+                  {courses.map((course, index) => (
+                    <span key={course} className="mr-2">
+                      {course}
+                      {index < courses.length - 1 ? ',' : ''}
+                    </span>
+                  ))}
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
     </>
   );
 } 
